ed1bbbfc16c7a24a4f2ea731aac560dc02b5c963
‘Lekky <lekkyjay@yahoo.com>
2024-07-27 21:25:43 +0200

    Implemented socket.io functionality with context api.

--- src/context/SocketContext.tsx
+++ src/context/SocketContext.tsx
@@ -12,10 +12,15 @@ export const SocketContextProvider = ({ children }: { children: React.ReactNode 
   const { currentUser } = useContext(AuthContext)
   const [socket, setSocket] = useState<Socket | null>(null)
 
+  //firsttime the application loads a socket connection is established with the server. a socketId is generated
+  //a new socketId is generated anytime the browser is reloaded or when the app is rerendered(not sure about this one)  
+  //this happens anytime i save changes to my code as the vite hot-reload causes my browser to reload
   useEffect(() => {
     setSocket(io('http://localhost:8000'))
   }, [])
 
+  //whenever a user signs in, a userId is associated with the socketId generated above
+  //associated userid remains thesame even when socketId changes. userId is persisted with localStorage.
   useEffect(() => {
   currentUser && socket?.emit('newUser', currentUser.id)
   }, [currentUser, socket])
